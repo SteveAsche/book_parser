2fccb840dff6b789ecb69203a7dda2545b2e2c24
SteveAsche <steve.asche@gmail.com>
2018-10-23 19:22:52 -0700

    worked on leftnode right node
    Still errors in the tree balancing

--- parse_a_book.py
+++ parse_a_book.py
@@ -8,31 +8,88 @@ class WordNode():
 		self.wordcount = 1
 
 	def __repr__(self):
-		return self.word
+		if self.leftword == None:
+			left = "No Left Word "
+		else:
+			left = " - left Exists "
+		if self.rightword == None:
+			right = " No Right Word "
+		else:
+			right = "- right Exists"
+		msg = self.word + left + right
+		return msg
 
 	def add_word(self, word):
 		new_word = WordNode(word)
 		return new_word
 
-	def find_word(self, startnode, nodeword):
-		if startnode.word == nodeword:
+	def insert_left_node(self, new_left_node):
+		if self.leftword.rightword != None:  # check to see if the node we are inserting to has a right word
+			is_there_a_right_word = True # if it does set the flag to True
+			compare_right = self.leftword.rightword.word #Also set the word in that right node to the string
+		else:
+			is_there_a_right_word = False  #if there is no right word, then set it to False
+
+
+		new_left_node.leftword = self.leftword #since we're inserting a left node, we know that the word value is less than self.word.  Point the left pointer on the new word to the leftword
+		self.leftword = new_left_node # Change the existing work to point at the left side do the new word.
+
+		if is_there_a_right_word:  #The left pointer is taken care of, now we need to see if the right pointer needs to point at the right pointer for the left child.
+			if compare_right > new_left_node.word: #if the compare_right word is greater than the word for the new node, then the new node rightword should point to it.
+				new_left_node.rightword = new_left_node.leftword.rightword # The right node for the left child should point branch off this new node.
+				new_left_node.leftword.rightword = None
+			else:
+				new_left_node.rightword = None
+
+	def insert_right_node(self, new_right_node):
+		if self.rightword.leftword != None:
+			is_there_a_left_word = True
+			compare_left = self.rightword.leftword.word
+		else:
+			is_there_a_left_word = False
+
+
+		new_right_node.rightword = self.rightword
+		self.rightword = new_right_node
+		print(new_right_node)
+		print("in new_right_node")
+		if is_there_a_left_word:
+			if compare_left < new_right_node.word:
+				new_right_node.leftword = new_right_node.rightword.leftword
+
+				new_right_node.rightword.leftword = None
+			else:
+				new_right_node.leftword = None
+
+
+
+
+
+	def find_word(self, startnode, nodeword):  #The first time through this is passed the firstnode which has "may" and no children.
+		if startnode.word == nodeword:  #if the node = the passed word, then add to the nodecount
 			startnode.wordcount += 1
-		elif startnode.word > nodeword:  # then it needs to go to the left
-			if startnode.leftword == None:
+		elif nodeword < startnode.word:  # then it needs to go to the left
+			print("L72 Going Left with " + nodeword + " compared to " + startnode.word)
+			if startnode.leftword == None: #check to see if there is a node on the left.  If not, add it there.
 				startnode.leftword = startnode.add_word(nodeword)
+				print("L75 added on the left - >" + nodeword)
 			elif startnode.leftword.word > nodeword:
 				newnode = self.add_word(nodeword)
-				newnode.leftword = startnode.leftword  # this part needs work
-				startnode.leftword = newnode
+				print("L78 inserted leftnode wiht -> " + nodeword)
+				self.insert_left_node(newnode)
+
 			else:
+				print("L81 Calling findword recursively with -> " + nodeword)
 				self.find_word(startnode.leftword, nodeword)
 		else:
+			print("L83 Going right! with " + nodeword + " compared to " + startnode.word)
 			if startnode.rightword == None:
 				startnode.rightword = startnode.add_word(nodeword)
+				print("L86 added on the right -> " + nodeword)
 			elif startnode.rightword.word < nodeword:
-				newnode = self.add_word(nodeword)
-				newnode.rightword = startnode.rightword
-				startnode.rightword = newnode
+				newnode = WordNode(nodeword)
+				self.insert_right_node(newnode)
+				
 			else:
 				self.find_word(startnode.rightword, nodeword)			
 
@@ -115,9 +172,9 @@ def count_words(filename):
 
 #filenames = ["Alice.txt", "moby_dick.txt", "little_women.txt", "siddhartha.txt"]
 words_list = []
-filename = "Alice.txt"
+filename = "Alice-short.txt"
 words_list = count_words(filename)
-valid_string = "abcdefghijlmnopqrstuvwxyz-" + "'"
+valid_string = "abcdefghijlmnopqrstuvwxyz-ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "'"
 firstnode = WordNode("may")
 print(firstnode)
 print(firstnode.leftword)
@@ -125,20 +182,23 @@ print(firstnode.rightword)
 print(firstnode.word)
 
 #print(words_list[100] + " " + words_list [1000] + " " + words_list[1])
+charcount = 1
 
 for oneword in words_list:
 	newstring = ""
 	for char in oneword:
 		if char in valid_string:
 			newstring += char
+	charcount += 1
+	print(charcount)
+	print("Calling find_word with " + newstring)		
+	firstnode.find_word(firstnode, newstring.lower())
 
-	firstnode.find_word(firstnode, newstring)
+
+#for i in range(1,20):
+#	print(words_list[i])
 
 
-print(firstnode)
-print(firstnode.leftword)
-print(firstnode.rightword)
-print(firstnode.word)
-print(firstnode.wordcount)
+
 #find_a_word(firstnode, "whale")
 traverse_word_tree(firstnode)
